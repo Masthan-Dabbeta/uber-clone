508d1c4ac043af5a76b93493c4c1158240e74f4d
Masthan-Dabbeta <149988879+Masthan-Dabbeta@users.noreply.github.com>
2024-03-29 12:11:36 +0530

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "ajay-mrcloudbook777" # Replace with your actual S3 bucket name
+    bucket = "manubucket2024" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
